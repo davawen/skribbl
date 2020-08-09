069dd6e87c4f7080c8745bac1d75e942d72b8da9
davawen <56738724+davawen@users.noreply.github.com>
2020-08-09 23:54:36 +0200

    Added p5.js

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ var app = express();
 
 var server = app.listen(8080);
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
+
+const socket = require("socket.io");
+var io = socket(server);
+
+io.sockets.on('connection', connected);
+
+function connected(socket)
+{
+    console.log("Connection");
+}
