0781ee5bc35d4d30b105ff17b84dd831b332981d
davawen <56738724+davawen@users.noreply.github.com>
2020-08-09 23:34:58 +0200

    Update server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+let handle = function(req, res)
+{
+    res.writeHead
+}
+
+const http = require('http');
+var server = http.createServer(handle);
+
+app.use(express.static('public'));
+
+console.log("Cool");
