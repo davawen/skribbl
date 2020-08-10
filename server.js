876c0d38d7d4499fc5d62e03930d254c49990781
davawen <56738724+davawen@users.noreply.github.com>
2020-08-10 02:13:46 +0200

    Added socket.io support

--- server.js
+++ server.js
@@ -1,25 +1,67 @@
-const fs = require('fs');
+const http = require('http');
 const port = 8080;
 
-function handleRequest(request, response)
-{
-    fs.readFile(__dirname + '/index.html',
-        function (err, data)
-        {
-            if (err)
+const fs = require('fs');
+const path = require('path');
+
+
+var server = http.createServer(handleRequest);
+server.listen(port);
+
+let io = require('socket.io').listen(server);
+
+io.sockets.on('connection',
+    function(socket)
+    {
+        console.log("Connection: " + socket.id);
+        
+        socket.on('disconnect',
+            function()
             {
-                response.writeHead(500);
-                return response.end('Error loading index.html');
+                console.log(socket.id + " disconnected");
             }
-            response.writeHead(200);
-            response.end(data);
+        );
+        
+        socket.on('mouse',
+            function(data)
+            {
+                console.log("Received: 'mouse' " + data.x + " " + data.y);
+                socket.broadcast.emit('mouse', data);
+            }
+        );
+    }
+);
+
+console.log('Server started on port ' + port);
+
+function handleRequest(req, res) {
+    var pathname = req.url;
+
+    if (pathname == '/') {
+    pathname = '/index.html';
+    }
+
+    var ext = path.extname(pathname);
+
+    // Map extension to file type
+    var typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+    };
+
+    var contentType = typeExt[ext] || 'text/plain';
+
+    fs.readFile(__dirname + pathname,
+        function (err, data) {
+            // if there is an error
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading ' + pathname);
+            }
+            // Otherwise, send the data, the contents of the file
+            res.writeHead(200,{ 'Content-Type': contentType });
+            res.end(data);
         }
     );
 }
-
-let http = require('http');
-
-let server = http.createServer(handleRequest);
-server.listen(port);
-
-console.log("Started on port " + port);
--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
-function setup() {
+let socket;
+
+function setup()
+{
 	createCanvas(400, 400);
+	
+	socket = io.connect();
+	
+	socket.on('mouse',
+		function(data)
+		{
+			// Draw a blue circle
+			fill(0);
+			noStroke();
+			circle(data.x,data.y,10);
+		}
+	);
+	
 }
 
-function draw() {
+function draw()
+{
 	background(200);
 	
-	noStroke();
+	let data = {
+		x: mouseX,
+		y: mouseY
+	};
+	// Send that object to the socket
+	socket.emit('mouse',data);
+	
+	/*noStroke();
 	fill(0);
-	circle(mouseX, mouseY, 10);
+	circle(mouseX, mouseY, 10);*/
 }
